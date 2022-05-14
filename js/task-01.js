a95e0c6a3d73c3f53b7b1a4a9441064026e51fa2
olypchuk <eragmont@gmail.com>
2022-05-14 21:25:06 +0300

    end

--- js/task-01.js
+++ js/task-01.js
@@ -1,13 +1,14 @@
 const list =document.querySelector('#categories');
-
 const listItems = list.querySelectorAll('li.item');
  console.log(`Number of categories :${listItems.length}`);
 
-
 const amount = () => {
-    for (let item of listItems) {
-  console.log("Category :",item.querySelector('h2').textContent);
-  console.log("Elements:",item.querySelector("ul").children.length);
+  for (let item of listItems) {
+    const firstEl = item.firstElementChild;
+    const lastEl = item.lastElementChild;
+
+  console.log("Category :",firstEl.textContent);
+  console.log("Elements:",lastEl.children.length);
     }   
  
 }
